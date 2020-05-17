1fdacc563795ceaf52e18e9ef56dd5bbab25a345
meestersumit <DchAdNGfyDMM7j8>
2020-05-17 16:04:55 -0700

    End of Section 10 (Updating API and in Angular app

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -26,6 +26,9 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { JwtHttpInterceptor } from './_services/jwt.interceptor';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { NgxImageGalleryModule } from 'ngx-image-gallery';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guard/prevent-unsaved-changes.guard';
 
 @NgModule({
   declarations: [
@@ -38,6 +41,7 @@ import { NgxImageGalleryModule } from 'ngx-image-gallery';
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
+    MemberEditComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,8 +58,10 @@ import { NgxImageGalleryModule } from 'ngx-image-gallery';
     ErrorInterceptorProvider,
     AlertifyService,
     AuthGuard,
+    PreventUnsavedChanges,
     UserService,
     MemberDetailResolver,
+    MemberEditResolver,
     MemberListResolver,
     { provide: HTTP_INTERCEPTORS, useClass: JwtHttpInterceptor, multi: true },
   ],
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -7,6 +7,9 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { AuthGuard } from './_guard/auth.guard';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guard/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,6 +27,12 @@ export const appRoutes: Routes = [
         path: 'members/:id',
         component: MemberDetailComponent,
         resolve: { user: MemberDetailResolver },
+      },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        resolve: { user: MemberEditResolver },
+        canDeactivate: [PreventUnsavedChanges],
       },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent },
